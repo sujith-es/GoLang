daed1e10e36363e143babd51caab87fcbbce6bd9
ssurendran <ssurendran@armor.com>
2017-10-22 10:24:31 +0530

    mutiple slice append

--- slices/slices.go
+++ slices/slices.go
@@ -7,8 +7,8 @@ import (
 func main() {
 
 	fmt.Println("Program to display Array and Slices\n")
-	//mySlices := make([]string, 10, 20)
-	mySlices := []int{2, 3, 4, 5, 6, 7}
+	mySlices := make([]int, 1, 4)
+	//mySlices := []int{2, 3, 4, 5, 6, 7}
 	fmt.Println("Actual mySlices Array")
 	fmt.Println(mySlices)
 	fmt.Printf("Length is: %d \nCapacity  is: %d", len(mySlices), cap(mySlices))
@@ -18,6 +18,16 @@ func main() {
 	fmt.Println("\nmySlices[0] Array")
 	fmt.Println(mySlices)
 	fmt.Println("\nmySlices[2:5] Array")
-	sliceOfSlice := mySlices[:5]
+	sliceOfSlice := mySlices[:2]
 	fmt.Println(sliceOfSlice)
+
+	fmt.Println("\nIncrease capacity of underlying Array used by slice")
+	for index := 1; index < 17; index++ {
+		mySlices = append(mySlices, index)
+		fmt.Printf("\nCapacity is: %d", cap(mySlices))
+	}
+	newSlices := []int{200, 300, 500}
+	mySlices = append(mySlices, newSlices...)
+	fmt.Println("\nnewSlices appended")
+	fmt.Println(mySlices)
 }
